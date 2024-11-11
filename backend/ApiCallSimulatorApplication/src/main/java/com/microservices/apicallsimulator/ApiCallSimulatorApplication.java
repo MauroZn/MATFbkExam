2fd690fad91a852dd2fb8d73e68d3afb660ef243
MauroZn <zanottimauro93@gmail.com>
2024-11-11 21:40:55 +0100

    Changed catalogUrl and purchaseUrl to take them from application.properties.

--- backend/ApiCallSimulatorApplication/src/main/java/com/microservices/apicallsimulator/ApiCallSimulatorApplication.java
+++ backend/ApiCallSimulatorApplication/src/main/java/com/microservices/apicallsimulator/ApiCallSimulatorApplication.java
@@ -1,21 +1,24 @@
 package com.microservices.apicallsimulator;
 
 import com.microservices.apicallsimulator.services.ApiCallService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.concurrent.TimeUnit;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiCallSimulatorApplication {
 
+    @Value("${catalog.url}")
+    static String catalogUrl;
+
+    @Value("${purchase.url}")
+    static String purchaseUrl;
+
     public static void main(String[] args) {
-
-        String catalogUrl = "http://localhost:9000/products";
-
-        String purchaseUrl = "http://localhost:9000/purchases";
-
         ApiCallService simulator = new ApiCallService(catalogUrl, purchaseUrl);
         simulator.startSimulation(2, TimeUnit.HOURS);
     }
